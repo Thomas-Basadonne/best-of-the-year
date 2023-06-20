c25e2a5b8d1c1666e17205c379390ee9f9044061
Thomas-Basadonne <thommybasa@gmail.com>
2023-06-20 15:37:35 +0200

    Initial commit

---
+++ src/main/java/com/example/bestOf/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.example.bestOf.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping("/home")
+    public String home(Model model) {
+        model.addAttribute("name", "Thomas");
+        return "home";
+    }
+
+
+}
